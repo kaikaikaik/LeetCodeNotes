f27ba576b160478e101fbf4ebe134922159f3e3e
Unknown <2796581265@qq.com>
2020-09-28 17:27:31 +0800

    桶排序解法
    buckets solution

--- LeetCode Notes/src/Algorithm/Sort/TopKFrequentElement.java
+++ LeetCode Notes/src/Algorithm/Sort/TopKFrequentElement.java
@@ -1,9 +1,6 @@
 package Algorithm.Sort;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 //Given a non-empty array of integers, return the k most frequent elements.
 //
@@ -23,6 +20,8 @@ import java.util.Map;
 //You can return the answer in any order.
 
 public class TopKFrequentElement {
+    Solution sol = new Solution();
+
 }
 
 class Solution{
@@ -31,11 +30,26 @@ class Solution{
         for(int num : nums){
             //getOrDefault,Map中存在key则使用key的值，否则使用defaultValue
             frequencyForNum.put(num, frequencyForNum.getOrDefault(num, 0) + 1);
-            List<Integer>[] buckets = new ArrayList[nums.length + 1];
-            for(int key : frequencyForNum.keySet()){
-                int frequency = frequencyForNum.get(key);
-
+        }
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+        for(int key : frequencyForNum.keySet()){
+            int frequency = frequencyForNum.get(key);
+            if(buckets[frequency] == null){
+                buckets[frequency] = new ArrayList<>();
+            }
+            buckets[frequency].add(key);
+        }
+        List<Integer> topK = new ArrayList<>();
+        for(int i = buckets.length - 1; i >= 0 && topK.size() < k; i--){
+            if(buckets[i] == null){
+                continue;
+            }
+            if(buckets[i].size() <= (k - topK.size())){
+                topK.addAll(buckets[i]);
+            }else {
+                topK.addAll(buckets[i].subList(0, k - topK.size()));
             }
         }
+        return topK;
     }
 }
